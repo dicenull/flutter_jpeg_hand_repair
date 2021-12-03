16ab36bc1928b0029a22abbd4f6c9f5bd8080d5c
dicenull <dicenull@users.noreply.github.com>
2021-12-03 12:39:56 +0000

    Automated deployment: Fri Dec  3 12:39:56 UTC 2021 bbe60858296264cf38b08d30c43fc733c6c970e4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "39cf28c242c235904c4f8504aa940dda",
-"/": "39cf28c242c235904c4f8504aa940dda",
+  "index.html": "459b2a10fd401b3603ca820965070582",
+"/": "459b2a10fd401b3603ca820965070582",
 "assets/FontManifest.json": "39018951616f8eadde6efcab968cc5f9",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/assets/images/dice.jpg": "85a5c2a84c2d0d81588555872e15fec6",
